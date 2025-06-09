16115dffdaf50f31ff081ad34f19fbdc9b4d0591
Lukas Schaefer <lukas@lschaefer.xyz>
2025-06-09 11:51:12 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<NcAppContent>
+		<div id="app_template">
+			<h1>Hello world!</h1>
+		</div>
+	</NcAppContent>
+</template>
+
+<script>
+import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
+
+export default {
+	name: 'App',
+	components: {
+		NcAppContent,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+#app_template {
+	display: flex;
+	justify-content: center;
+	margin: 16px;
+}
+</style>
